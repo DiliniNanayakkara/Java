6b1f5beb2bf66abcde6d702142817cf37d95be09
SandaniWimalasiri <sandaniwimalasiri@gmail.com>
2020-10-31 22:43:50 +0530

    Added java file

---
+++ p12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class p12
+{
+	public static void main(String[] args)
+	{
+		int count =1;
+		
+		while(count <=10)
+		{
+			System.out.println("Sri lanka");
+			count++;
+			
+		}
+	}
+}
